7cbb2648f649c1fb628d620fa1724b04655d1a5d
josemca1105 <jm.calderon1105@gmail.com>
2024-02-14 13:50:13 -0400

    sala 1 sin terminar

--- pagina_cine-master/front/src/app/Pages/sala-1/sala-1.component.ts
+++ pagina_cine-master/front/src/app/Pages/sala-1/sala-1.component.ts
@@ -1,14 +1,30 @@
-import { NgClass, NgFor, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sala-1',
   standalone: true,
-  imports: [NgFor, NgClass, NgSwitch, NgIf, NgSwitchCase, NgSwitchDefault],
+  imports: [],
   templateUrl: './sala-1.component.html',
   styleUrl: './sala-1.component.css'
 })
 export class Sala1Component {
-
-
+  movieList: any[] = [
+    {
+      movieName: 'John Wick',
+      ticketRate: '5',
+      movieBannerImage: '../../../assets/1/john01.jpg',
+      description: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+      // shows: ['09:00am - 12pm', '02:00pm - 05:00pm'],
+      seatRows: [
+        {row: 'A', nOfSeats: 8},
+        {row: 'B', nOfSeats: 8},
+        {row: 'C', nOfSeats: 8},
+        {row: 'D', nOfSeats: 8},
+        {row: 'E', nOfSeats: 8},
+        {row: 'F', nOfSeats: 8},
+        {row: 'G', nOfSeats: 8},
+        {row: 'H', nOfSeats: 10},
+      ]
+    },
+  ];
 }
