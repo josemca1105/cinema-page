1cc3bea83ce4c7693aab5f2e66acaaa3e0477130
josemca1105 <jm.calderon1105@gmail.com>
2024-02-15 01:40:18 -0400

    tabla peliculas sin imagenes

--- pagina_cine-master/front/src/app/Pages/peliculas-page/peliculas-page.component.ts
+++ pagina_cine-master/front/src/app/Pages/peliculas-page/peliculas-page.component.ts
@@ -1,10 +1,29 @@
 import { NgFor, CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgxPaginationModule } from 'ngx-pagination';
 import jsPDF from 'jspdf';
 import autoTable from 'jspdf-autotable';
 
+interface Movie {
+  id: string;
+  imagen: string;
+  nombre: string;
+  descripcion: string;
+  duracion: string;
+  genero: string;
+  estreno: string;
+}
+
+interface Column {
+  field: string;
+  header: string;
+}
+
+interface ExportColumn {
+  title: string;
+  dataKey: string;
+}
 
 @Component({
   selector: 'app-peliculas-page',
@@ -13,7 +32,7 @@ import autoTable from 'jspdf-autotable';
   templateUrl: './peliculas-page.component.html',
   styleUrl: './peliculas-page.component.css'
 })
-export class PeliculasPageComponent {
+export class PeliculasPageComponent implements OnInit {
   p: number = 1;
 
   imagen: string = '';
@@ -26,14 +45,32 @@ export class PeliculasPageComponent {
   currentPeliculaID = '';
   peliculaArray: any[] = [];
 
+  movies: Movie[] = [];
+  cols!: Column[];
+  exportColumns!: ExportColumn[];
+
   constructor(private http: HttpClient) {
     this.getAllPelicula();
   }
+
+  ngOnInit(): void {
+    this.cols = [
+      { field: 'imagen', header: 'Imagen' },
+      { field: 'nombre', header: 'Nombre' },
+      { field: 'descripcion', header: 'Descripcion' },
+      { field: 'duracion', header: 'Duracion' },
+      { field: 'genero', header: 'Genero' },
+      { field: 'estreno', header: 'Estreno' },
+    ];
+
+    this.exportColumns = this.cols.map((col) => ({ title: col.header, dataKey: col.field }));
+  };
 
   getAllPelicula() {
     this.http.get("http://127.0.0.1:8000/api/peliculas").subscribe((resultData: any)=> {
         // console.log(resultData);
         this.peliculaArray = resultData;
+        this.movies = resultData;
     });
   }
 
@@ -49,30 +86,86 @@ export class PeliculasPageComponent {
     });
   }
 
+  // generatePDF() {
+  //   const margins = {
+  //     top: 30,
+  //     bottom: 30,
+  //     left: 10,
+  //     right: 10
+  //   };
+
+  //   const doc = new jsPDF();
+  //   doc.setFont('Times');
+  //   doc.setFontSize(18);
+  //   doc.text('Pruebaaaa', margins.left, margins.top);
+
+  //   doc.setFontSize(12);
+  //   doc.text('texto de pruebaaaa', margins.left, 50);
+
+  //   doc.addImage(
+  //     '../../../assets/1/john01.jpg',
+  //     margins.left,
+  //     70,
+  //     20,
+  //     30
+  //   );
+
+  //   doc.save('peliculas.pdf'); // Guardar PDF
+  // }
+  // generatePDF() {
+  //   const margins = {
+  //     top: 15,
+  //     bottom: 15,
+  //     left: 10,
+  //     right: 10
+  //   };
+
+  //   const doc = new jsPDF();
+  //   doc.setFont('Times');
+  //   doc.setFontSize(18);
+  //   doc.text('Listado de Peliculas', margins.left, margins.top);
+
+  //   // Get all movie data, not just the current page
+  //   const allPeliculaData = this.peliculaArray;
+
+  //   // Create the table header
+  //   const tableHeaders = ['Imagen', 'Nombre', 'Descripcion', 'Duracion', 'Genero', 'Estreno'];
+
+  //   // Create the table data
+  //   const tableData = allPeliculaData.map(pelicula => [
+  //     this.getImageUrl(pelicula.id),
+  //     pelicula.nombre,
+  //     pelicula.descripcion,
+  //     pelicula.duracion,
+  //     pelicula.genero,
+  //     pelicula.estreno
+  //   ]);
+
+  //   // Generate the PDF table
+  //   autoTable(doc, {
+  //     startY: 50,
+  //     // head: tableHeaders,
+  //     body: tableData,
+  //     styles: {
+  //       halign: 'left',
+  //       cellPadding: 5
+  //     }
+  //   });
+
+  //   doc.save('peliculas.pdf');
+  // }
   generatePDF() {
-    const margins = {
-      top: 30,
-      bottom: 30,
-      left: 10,
-      right: 10
-    };
-
-    const doc = new jsPDF();
-    doc.setFont('Times');
-    doc.setFontSize(18);
-    doc.text('Pruebaaaa', margins.left, margins.top);
-
-    doc.setFontSize(12);
-    doc.text('texto de pruebaaaa', margins.left, 50);
-
-    doc.addImage(
-      '../../../assets/1/john01.jpg',
-      margins.left,
-      70,
-      20,
-      30
-    );
-
-    doc.save('peliculas.pdf'); // Guardar PDF
+    import('jspdf').then((jsPDF) => {
+      import('jspdf-autotable').then((x) => {
+        const doc = new jsPDF.default('p', 'px', 'a4');
+        doc.setFontSize(20);
+        doc.text('Listado de Peliculas', 10, 10);
+        (doc as any).autoTable(
+          this.exportColumns,
+          this.movies
+        );
+        doc.save('peliculas.pdf');
+      })
+    })
   }
 }
--- pagina_cine-master/front/src/app/Pages/sala-1/sala-1.component.ts
+++ pagina_cine-master/front/src/app/Pages/sala-1/sala-1.component.ts
@@ -19,11 +19,59 @@ export class Sala1Component {
         {row: 'A', nOfSeats: 8},
         {row: 'B', nOfSeats: 8},
         {row: 'C', nOfSeats: 8},
-        {row: 'D', nOfSeats: 8},
-        {row: 'E', nOfSeats: 8},
-        {row: 'F', nOfSeats: 8},
-        {row: 'G', nOfSeats: 8},
-        {row: 'H', nOfSeats: 10},
+        {row: 'D', nOfSeats: 10},
+      ]
+    },
+    {
+      movieName: 'Interestelar',
+      ticketRate: '5',
+      movieBannerImage: '../../../assets/2/main.jpg',
+      description: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+      // shows: ['09:00am - 12pm', '02:00pm - 05:00pm'],
+      seatRows: [
+        {row: 'A', nOfSeats: 8},
+        {row: 'B', nOfSeats: 8},
+        {row: 'C', nOfSeats: 8},
+        {row: 'D', nOfSeats: 10},
+      ]
+    },
+    {
+      movieName: 'Avengers: Infinity War',
+      ticketRate: '5',
+      movieBannerImage: '../../../assets/3/infi.jpg',
+      description: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+      // shows: ['09:00am - 12pm', '02:00pm - 05:00pm'],
+      seatRows: [
+        {row: 'A', nOfSeats: 8},
+        {row: 'B', nOfSeats: 8},
+        {row: 'C', nOfSeats: 8},
+        {row: 'D', nOfSeats: 10},
+      ]
+    },
+    {
+      movieName: 'El Exorcista',
+      ticketRate: '5',
+      movieBannerImage: '../../../assets/4/exor.jpg',
+      description: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+      // shows: ['09:00am - 12pm', '02:00pm - 05:00pm'],
+      seatRows: [
+        {row: 'A', nOfSeats: 8},
+        {row: 'B', nOfSeats: 8},
+        {row: 'C', nOfSeats: 8},
+        {row: 'D', nOfSeats: 10},
+      ]
+    },
+    {
+      movieName: 'La La Land',
+      ticketRate: '5',
+      movieBannerImage: '../../../assets/5/poster.jpg',
+      description: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+      // shows: ['09:00am - 12pm', '02:00pm - 05:00pm'],
+      seatRows: [
+        {row: 'A', nOfSeats: 8},
+        {row: 'B', nOfSeats: 8},
+        {row: 'C', nOfSeats: 8},
+        {row: 'D', nOfSeats: 10},
       ]
     },
   ];
